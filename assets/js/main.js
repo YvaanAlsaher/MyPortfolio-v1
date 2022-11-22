06edbcf5ef9537798821f2b5410486762353ab24
YvaanAlsaher <45407023+YvaanAlsaher@users.noreply.github.com>
2022-11-22 19:48:04 +0100

    loader

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,44 @@
-/*==================== MENU SHOW Y HIDDEN ====================*/
+function loader_percentage() {
+    let loader_percen = { percen: 0 },
+      percenDisplay = document.getElementById("percenDisplay");
+  
+    function showpercen() {
+      percenDisplay.innerHTML = loader_percen.percen.toFixed(1);
+  
+      if (loader_percen.percen.toFixed(2) >= 100) {
+        $(".loader").fadeOut();
+        $("body").css('overflow-y','auto');
+        $("main").css('opacity','1');
+      }
+    }
+  
+    function imgLoaded() {
+      c += 1;
+      let perc = ((100 / tot) * c) << 0;
+  
+      let tween = TweenLite.to(loader_percen, 3, {
+        percen: perc,
+        onUpdate: showpercen,
+      });
+    }
+  
+    let img = document.images,
+      c = 0,
+      tot = img.length;
+  
+    for (let i = 0; i < tot; i++) {
+      let tImg = new Image();
+      tImg.onload = imgLoaded;
+      tImg.onerror = imgLoaded;
+      tImg.src = img[i].src;
+      console.log(` Image ${i} est correctement chargée`);
+      console.log("tImg",i);
+    }
+  }
+  
+  loader_percentage();
+
+/*==================== MENU SHOW AND HIDDEN ====================*/
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
         navClose = document.getElementById('nav-close');
